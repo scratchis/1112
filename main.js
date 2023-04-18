be16ed641069e52711bfb2bd1f3a27b2cb9a6d8a
scratchis <103792439+scratchis@users.noreply.github.com>
2023-04-18 19:27:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -13,4 +13,18 @@ function take_snapshot()
     })
 }
 console.log("ml5 version:", ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Yzp7VvEry/model.json");
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Yzp7VvEry/modal.json");
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
